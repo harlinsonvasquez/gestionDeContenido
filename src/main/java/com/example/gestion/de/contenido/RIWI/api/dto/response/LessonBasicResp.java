33d18a1f839439a545c8f4ab541061975e34960f
harlinsonvasquez <harvack25@gmail.com>
2024-06-05 13:59:22 -0500

    creancion de service lesson y multimedia

---
+++ src/main/java/com/example/gestion/de/contenido/RIWI/api/dto/response/LessonBasicResp.java
@@ -0,0 +1,21 @@
+package com.example.gestion.de.contenido.RIWI.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LessonBasicResp {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private List<MultimediaResp> multimedia;
+}
