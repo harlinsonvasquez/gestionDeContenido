36c87cd36251e9d64a7d9f9cd3edf7b484821021
harlinsonvasquez <harvack25@gmail.com>
2024-06-05 07:43:27 -0500

    creacion y configuracion de la entidad lesson

--- src/main/java/com/example/gestion/de/contenido/RIWI/domain/entities/ClassEntity.java
+++ src/main/java/com/example/gestion/de/contenido/RIWI/domain/entities/ClassEntity.java
@@ -32,6 +32,6 @@ public class ClassEntity {
 
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
-    @OneToMany(mappedBy = "class",cascade = CascadeType.ALL,orphanRemoval = false,fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "classEntity",cascade = CascadeType.ALL,orphanRemoval = false,fetch = FetchType.LAZY)
     private List<Lesson> lessons;
 }
